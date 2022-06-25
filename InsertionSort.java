04e7c50d1018bef8ca1ecffc3ce491633f8cdc3d
vishangi <vishangid@gmail.com>
2022-06-25 23:53:01 +0530

    Added : Print integer sorted order using Insertion Sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -3,16 +3,16 @@ package com.bridgelabz;
 public class InsertionSort {							 
 	 //Function to sort array using insertion sort										
 	public void sorting(int[] arr) {                     
-		int len = arr.length;							//Before sorting
-		for(int i = 0; i<len ; i++){ 					// 8 4 1 5 9 6
-			int temp = arr[i];							// 8 | 4 1 5 9 6
-			int j = i-1;								// 4 8 | 1 5 9 6
-			while(j>=0 && arr[j]>temp) {  				// 1 4 8 | 5 9 6             
-				arr[j+1] = arr[j];						// 1 4 5 8 | 9 6				
-				j--;									// 1 4 5 8 9 | 6
+		int len = arr.length;							
+		for(int i = 0; i<len ; i++){ 				
+			int temp = arr[i];							
+			int j = i-1;								
+			while(j>=0 && arr[j]>temp) {  				           
+				arr[j+1] = arr[j];										
+				j--;									
 			}
-			arr[j+1] = temp;							//Before sorting
-		}                                               // 1 4 5 6 8 9
+			arr[j+1] = temp;							
+		}                                              
 	}
 	//Function to print array
 	public void printArray(int[] arr) {
