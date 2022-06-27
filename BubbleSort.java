cf31460d7f1e954e1d2bce77f037f2c60fb41a15
vishangi <vishangid@gmail.com>
2022-06-27 19:29:36 +0530

    modified : Print integer sorted order using Bubble Sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,34 +2,38 @@ package com.bridgelabz;
 //Java program for implementation of Bubble Sort
 public class BubbleSort {
 	 //Function to sort array using bubble sort
-	private static void sorting(int[] arr) {
-		int len = arr.length;
-		int temp;
-		for (int i = 0; i < len - 1; i++) {							
-			for (int j = 0; j < len - 1 - i; j++) {					
-				if (arr[j] > arr[j + 1]) {							
-					temp = arr[j];								
+	static void sorting(String[] arr) {
+		
+		String temp;
+		for (int i = 0; i < arr.length-1; i++) {							
+			for (int j = 0; j < arr.length- i-1; j++) {					
+				if (arr[j].compareTo(arr[j + 1]) > 0) {							
+					temp = arr[j];									
 					arr[j] = arr[j + 1];							
 					arr[j + 1] = temp;
 				}													
 			}														
-		}															 
+		}														
 	}
 	//Function to print array										
-	private static void printArray(int[] arr) {						
-		int len = arr.length;										
-		for (int i = 0; i < len; i++) {							
+	static void printArray(String[] arr) {						
+												
+		for (int i = 0; i < arr.length; i++) {								
 			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
 	}
-
+	//Main method
 	public static void main(String[] args) {
-		int[] arr = { 5, 2, 4, 1, 3 };
 		BubbleSort bubbleSort = new BubbleSort();
+		
+		String [] arr = { "vish", "madhu", "priya", "ashu", "nisha" };
+		
 		System.out.println("Before sorting");
 		BubbleSort.printArray(arr);
+		
 		BubbleSort.sorting(arr);
+		
 		System.out.println("After sorting");
 		BubbleSort.printArray(arr);
 	}
