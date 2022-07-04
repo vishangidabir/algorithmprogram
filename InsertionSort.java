3b2c43bb660047acbe1bee681cfcba9675a9dd53
vishangi <vishangid@gmail.com>
2022-07-04 13:16:55 +0530

    Modified : Print string sorted order using insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,40 +2,35 @@ package com.bridgelabz;
 //Java program for implementation of Insertion Sort
 public class InsertionSort {							 
 	 //Function to sort array using insertion sort										
-	public void sorting(String [] arr) {                     
+	public void sorting(String [] array) {
 									
-		for(int i = 1; i<arr.length ; i++){ 					
-			String temp = arr[i];							
+		for(int i = 1; i<array.length ; i++){
+			String temp = array[i];
 			int j = i-1;								
-			while(j >= 0 && arr[j].compareTo(temp) > 0) {  				           
-				arr[j+1] = arr[j];								
+			while(j >= 0 && array[j].compareTo(temp) > 0) {
+				array[j+1] = array[j];
 				j--;									
 			}
-			arr[j+1] = temp;						
+			array[j+1] = temp;
 		}                                               
 	}
 	//Function to print array
-	public void printArray(String[] arr) {
-		int len = arr.length;
+	public void printArray(String[] array) {
+		int len = array.length;
 		for(int i = 0; i<len ; i++ ) {
-			System.out.print(arr[i] + " ");
+			System.out.print(array[i] + " ");
 		}
 		System.out.println();
 	}
-
 	public static void main(String[] args) {
-		String [] arr = {"vish","madhu","priya","ashu","nisha"};
+		String [] array = {"vish","madhu","priya","ashu","nisha"};
 		InsertionSort insertionSort = new InsertionSort();
 		
 		System.out.println("Before sorting");
-		insertionSort.printArray(arr);
+		insertionSort.printArray(array);
 		
-		insertionSort.sorting(arr);
+		insertionSort.sorting(array);
 		System.out.println("After sorting");
-		insertionSort.printArray(arr);
+		insertionSort.printArray(array);
 	}
-
-	
-	
-
 }
