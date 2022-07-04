efff5b2d0eb57465b6332304f4d6f178dd06549d
vishangi <vishangid@gmail.com>
2022-07-04 13:34:18 +0530

    Modified : find prime number between 0 - 1000 number

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class PrimeNumber {
-	static int low, upp, num, temp, i, count;
+	static int lower, upper, number, temp, i, count;
 
 	public void isPrime() {
 		Scanner scanner = new Scanner(System.in);
@@ -12,16 +12,16 @@ public class PrimeNumber {
 		System.out.println("Enter the upper limit");
 		int upp = scanner.nextInt();
 		System.out.println("PrimeNumbers between " + low + " to " + upp);
-		for (num = low; num <= upp; num++) {
+		for (number = low; number <= upp; number++) {
 			count = 0;
-			temp = num;
+			temp = number;
 			for (i = 1; i <= temp; i++) {
 				if (temp % i == 0) {
 					count++;
 				}
 			}
 			if (count == 2) {
-				System.out.print(num + " ");
+				System.out.print(number + " ");
 			}
 		}
 	}
