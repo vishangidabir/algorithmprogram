c6582dbf9c23e3ed013ca609c6124d148be8a92d
vishangi <vishangid@gmail.com>
2022-07-04 13:11:53 +0530

    Modified : Print string sorted using bubble sort in generic way

--- BubbleSortGeneric.java
+++ BubbleSortGeneric.java
@@ -4,23 +4,23 @@ package com.bridgelabz;
 public class BubbleSortGeneric<T> {
 
 	// Function to print array
-	public static <T> void printArray(T[] arr) {
-		for (T t : arr) {
+	public static <T> void printArray(T[] array) {
+		for (T t : array) {
 			System.out.print(t + " ");
 		}
 		System.out.print(" ");
 	}
 
 	// Function to sort array using bubble sort
-	public <T extends Comparable<T>> T bubbleSort(T[] arr) {
-		int len = arr.length;
+	public <T extends Comparable<T>> T bubbleSort(T[] array) {
+		int length = array.length;
 		T temp;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length - i - 1; j++) {
-				if (arr[j].compareTo(arr[j + 1]) > 0) {
-					temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length - i - 1; j++) {
+				if (array[j].compareTo(array[j + 1]) > 0) {
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
 				}
 			}
 		}
@@ -30,24 +30,24 @@ public class BubbleSortGeneric<T> {
 	// Main method
 	public static void main(String[] args) {
 
-		String[] arrayOfString = { "vish", "madhu", "priya", "ashu", "nisha" };
+		String[] string = { "vish", "madhu", "priya", "ashu", "nisha" };
 		BubbleSortGeneric<String> stringSorter = new BubbleSortGeneric<>();
 		System.out.println("String Bubble sorting");
 		System.out.println("\nBefore sorting :");
-		stringSorter.printArray(arrayOfString);
-		stringSorter.bubbleSort(arrayOfString);
+		stringSorter.printArray(string);
+		stringSorter.bubbleSort(string);
 		System.out.println("\nAfter sorting :");
-		stringSorter.printArray(arrayOfString);
+		stringSorter.printArray(string);
 		System.out.println();
 
 		System.out.println("\nInteger Bubble sorting");
-		Integer[] arrOfInteger = { 8, 4, 1, 5, 9, 6 };
+		Integer[] array = { 8, 4, 1, 5, 9, 6 };
 		BubbleSortGeneric<Integer> integerSorter = new BubbleSortGeneric<>();
 		System.out.println("\nBefore sorting :");
-		integerSorter.printArray(arrOfInteger);
-		integerSorter.bubbleSort(arrOfInteger);
+		integerSorter.printArray(array);
+		integerSorter.bubbleSort(array);
 		System.out.println("\nAfter sorting :");
-		integerSorter.printArray(arrOfInteger);
+		integerSorter.printArray(array);
 
 	}
 }
