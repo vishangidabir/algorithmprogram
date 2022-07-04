75ea10af740522c4d7c0daef29b3965a525632a7
vishangi <vishangid@gmail.com>
2022-07-04 13:17:38 +0530

    Modified : Print string sorted order using insertion sort in generic way

--- InsertionSortGeneric.java
+++ InsertionSortGeneric.java
@@ -1,49 +1,48 @@
 package com.bridgelabz;
 
 public class InsertionSortGeneric<T> {
-
 	// Function to print array
-		public static <T> void printArray(T[] arr) {
-			for (T t : arr) {
+		public static <T> void printArray(T[] array) {
+			for (T t : array) {
 				System.out.print(t + " ");
 			}
 			System.out.print(" ");
 		}
 	
 	 //Function to sort array using insertion sort										
-		public <T extends Comparable<T>> T insertionSort(T[] arr) {                     
+		public <T extends Comparable<T>> T insertionSort(T[] array) {
 										
-			for(int i = 1; i<arr.length ; i++){ 					
-				T temp = arr[i];							
+			for(int i = 1; i<array.length ; i++){
+				T temp = array[i];
 				int j = i-1;								
-				while(j >= 0 && arr[j].compareTo(temp) > 0) {  				           
-					arr[j+1] = arr[j];								
+				while(j >= 0 && array[j].compareTo(temp) > 0) {
+					array[j+1] = array[j];
 					j--;									
 				}
-				arr[j+1] = temp;						
+				array[j+1] = temp;
 			}
 			return null;                                               
 		}
 		
 		// Main method
 		public static void main(String[] args) {
-			String[] arrayOfString = { "vish", "madhu", "priya", "ashu", "nisha" };
+			String[] string = { "vish", "madhu", "priya", "ashu", "nisha" };
 			InsertionSortGeneric<String> stringSorter = new InsertionSortGeneric<>();
 			System.out.println("String Insertion sorting");
 			System.out.println("\nBefore sorting :");
-			stringSorter.printArray(arrayOfString);
-			stringSorter.insertionSort(arrayOfString);
+			stringSorter.printArray(string);
+			stringSorter.insertionSort(string);
 			System.out.println("\nAfter sorting :");
-			stringSorter.printArray(arrayOfString);
+			stringSorter.printArray(string);
 			System.out.println();
 
 			System.out.println("\nInteger Insertion sorting");
-			Integer[] arrOfInteger = { 8, 4, 1, 5, 9, 6 };
+			Integer[] array = { 8, 4, 1, 5, 9, 6 };
 			InsertionSortGeneric<Integer> integerSorter = new InsertionSortGeneric<>();
 			System.out.println("\nBefore sorting :");
-			integerSorter.printArray(arrOfInteger);
-			integerSorter.insertionSort(arrOfInteger);
+			integerSorter.printArray(array);
+			integerSorter.insertionSort(array);
 			System.out.println("\nAfter sorting :");
-			integerSorter.printArray(arrOfInteger);
+			integerSorter.printArray(array);
 		}
 	}
